87e41d4c15614e2c0dd9e705ac9596e5e1db1837
mikamarisol <57419248+mikamarisol@users.noreply.github.com>
2023-08-20 23:02:15 +0100

    2022 day 6

---
+++ 2022/day6.py
@@ -0,0 +1,26 @@
+packet_marker_length = 4
+message_marker_length = 14
+
+
+def characters_to_first_marker(marker_length):
+    signal = read_signal()
+    i = marker_length
+    while not marker_found(i, signal, marker_length):
+        i += 1
+    return i
+
+
+def read_signal():
+    with open('resources/signal.txt') as signal:
+        signal = signal.readline()
+        return signal
+
+
+def marker_found(end, signal, marker_length):
+    start = end - marker_length
+    return len(set(signal[start:end])) == marker_length
+
+
+if __name__ == '__main__':
+    print(characters_to_first_marker(packet_marker_length))
+    print(characters_to_first_marker(message_marker_length))
