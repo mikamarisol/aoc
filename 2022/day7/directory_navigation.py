8940574a66fe76844659e498eccb9e69f76524b2
mikamarisol <57419248+mikamarisol@users.noreply.github.com>
2023-08-23 14:55:24 +0100

    naming refactor

--- 2022/day7/directory_navigation.py
+++ 2022/day7/directory_navigation.py
@@ -1,40 +1,43 @@
 import re
 
 from patterns import *
-from bigtree import Node
+from directory import Directory
 
 
-class DirectoryTree:
+def read_instructions(directory_navigation_file):
+    with open(directory_navigation_file) as file:
+        return file.read(20480).splitlines()[1:]
 
-    def __init__(self, directory_navigation_file):
-        with open(directory_navigation_file) as file:
-            self.directory_navigation = file.read(20480).splitlines()[1:]
-        self.tree = self.tree_from_instructions()
 
-    def tree_from_instructions(self):
+def tree_from_instructions(directory_navigation):
+    root = Directory('/')
+    current_dir = root
+    for instruction in directory_navigation:
 
-        root = Node('/')
-        current_dir = root
-        for instruction in self.directory_navigation:
-            if GO_TO_PARENT_PATTERN.match(instruction):
-                print(instruction, 'going up')
-            elif GO_TO_NODE_PATTERN.match(instruction):
-                name = GO_TO_NODE_PATTERN.match(instruction).group('name')
-                # current_dir = current_dir.children
-                print(instruction, 'making node', name)
-            elif LIST_CHILDREN_PATTERN.match(instruction):
-                print(instruction)
-            elif LEAF_PATTERN.match(instruction):
-                size = LEAF_PATTERN.match(instruction).group('size')
-                name = LEAF_PATTERN.match(instruction).group('name')
-                print(instruction, 'leaf size {} name {}'.format(size, name))
-            elif DIRECTORY_PATTERN.match(instruction):
-                name = DIRECTORY_PATTERN.match(instruction).group('name')
-                print(instruction, name)
-            # print(current_dir)
+        if GO_TO_PARENT_PATTERN.match(instruction):
+            current_dir = current_dir.parent
+            # print(instruction, 'going up')
+
+        elif GO_TO_DIR_PATTERN.match(instruction):
+            name = GO_TO_DIR_PATTERN.match(instruction).group('name')
+            # current_dir = current_dir.children
+            # print(instruction, 'going to node', name)
+
+        elif LIST_CHILDREN_PATTERN.match(instruction):
+            # print(instruction)
+
+        elif FILE_PATTERN.match(instruction):
+            size = FILE_PATTERN.match(instruction).group('size')
+            name = FILE_PATTERN.match(instruction).group('name')
+            # print(instruction, 'leaf size {} name {}'.format(size, name))
+
+        elif DIR_PATTERN.match(instruction):
+            name = DIR_PATTERN.match(instruction).group('name')
+            # print(instruction, name)
+        # print(current_dir)
 
 
 if __name__ == '__main__':
-    terminal_output_file = '../../resources/directory_navigation_output.txt'
-    tree = DirectoryTree(terminal_output_file)
-    print(tree.tree_from_instructions())
+    instructions_file = '../../resources/directory_navigation_output.txt'
+    instructions = read_instructions(instructions_file)
+    print(tree_from_instructions(instructions))
