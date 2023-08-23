c0877257dbe158f042093d5f881f622bc88d8f67
mikamarisol <57419248+mikamarisol@users.noreply.github.com>
2023-08-23 23:43:38 +0100

    day 8 task 2

--- 2022/day8.py
+++ 2022/day8.py
@@ -11,8 +11,9 @@ def read_forest_map():
     # forest[j][i] = x_i, y_j
     heights = np.array([[int(forest[j][i]) for i in range(n)] for j in range(m)])
     # already know edges are visible
-    visibles = np.array([[1 if i in (0, n - 1) or j in (0, m - 1) else 0 for i in range(n)] for j in range(m)])
-    forest_map = np.stack([heights, visibles])
+    visibles = np.array([[1 if i in (0, n-1) or j in (0, m-1) else 0 for i in range(n)] for j in range(m)])
+    scenic_scores = np.array([[0 if i in (0, n-1) or j in (0, m-1) else 1 for i in range(n)] for j in range(m)])
+    forest_map = np.stack([heights, visibles, scenic_scores])
     return forest_map, n, m
 
 
@@ -23,7 +24,6 @@ def how_many_visible(map, n, m):
                 if is_visible(map, i, j, direction):
                     map = set_visible(map, i, j)
                     break
-    print(map[1])
     return np.count_nonzero(map[1] == 1)
 
 
@@ -47,10 +47,42 @@ def set_visible(map, i, j):
     return map
 
 
+def best_scenic_score(map, n, m):
+    for i in range(1, n-1):
+        for j in range(1, m-1):
+            map[2, j, i] = scenic_score(map, i, j)
+    return np.max(map[2])
+
+
+def number_trees_visible(map, i, j, direction):
+    if direction == 'N':
+        view = map[0, j-1::-1, i]
+    elif direction == 'E':
+        view = map[0, j, i+1:]
+    elif direction == 'S':
+        view = map[0, j+1:, i]
+    elif direction == 'W':
+        view = map[0, j, i-1::-1]
+    if (map[0, j, i] > view).all():
+        count = len(view)
+    else:
+        count = np.where(view >= map[0, j, i])[0][0] + 1
+    return count
+
+
+def scenic_score(map, i, j):
+    score = 1
+    for direction in ['N', 'E', 'S', 'W']:
+        score *= number_trees_visible(map, i, j, direction)
+    return score
+
+
+
 if __name__ == '__main__':
     # forest[k][j][i] = x_i, y_j, z_k
     # k = 0 -> heights
     # k = 1 -> visibles
+    # k = 2 -> scores
     map, n, m = read_forest_map()
-    print(map[0])
     print(how_many_visible(map, n, m))
+    print(best_scenic_score(map, n, m))
