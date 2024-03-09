27ee0eaf90dc5ca7fa557af62e20afdc9a1915b0
mikamarisol <57419248+mikamarisol@users.noreply.github.com>
2024-03-09 01:15:30 +0000

    day 10 start part 2

--- 2022/day10.py
+++ 2022/day10.py
@@ -15,22 +15,42 @@ def read_cathode_ray_instructions():
     return cycle_instructions
 
 
-def get_strength_at_cycle(cycle_instructions, cycle):
+def apply_cpu_cycle_instructions(cycle_instructions, cycle):
     x = 1 + sum(cycle_instructions[:cycle])
-    return cycle+1, x, (cycle+1) * x
+    return cycle + 1, x, (cycle + 1) * x
 
 
 def sum_signal_strengths_at_intervals(cycle_instructions, start, interval):
     signal_strength = 0
-    for c in range(len(cycle_instructions)+1):
+    for c in range(len(cycle_instructions) + 1):
         if c + 1 == start or (c + 1) % interval == start:
-            print(c+1, get_strength_at_cycle(cycle_instructions, c))
-            signal_strength += get_strength_at_cycle(cycle_instructions, c)[2]
+            print(c + 1, apply_cpu_cycle_instructions(cycle_instructions, c))
+            signal_strength += apply_cpu_cycle_instructions(cycle_instructions, c)[2]
     return signal_strength
 
 
+def apply_crt_cycle_instructions(cycle_instructions):
+    crt_output = []
+    for c in range(len(cycle_instructions) + 1):
+        print(c+1, draw_sprite(apply_cpu_cycle_instructions(cycle_instructions, c)[1]))
+        cycle, x, signal_strength = apply_cpu_cycle_instructions(cycle_instructions, c)
+        sprite = draw_sprite(x)
+        print('checking sprite', sprite[cycle - 1:cycle + 2])
+        if '#' in sprite[cycle - 1:cycle+ 2]:
+            crt_output.append('#')
+        else:
+            crt_output.append('.')
+        print('crt', crt_output)
+    return crt_output
+
+
+def draw_sprite(x):
+    sprite = ['#' if x-1 <= i <= x+1 else '.' for i in range(40)]
+    return sprite
+
 if __name__ == '__main__':
     cycle_instructions = read_cathode_ray_instructions()
-    print(cycle_instructions)
-    print('cycle, x, strength')
-    print(sum_signal_strengths_at_intervals(cycle_instructions, 20, 40))
+    # print(cycle_instructions)
+    # print('cycle, x, strength')
+    # print(sum_signal_strengths_at_intervals(cycle_instructions, 20, 40))
+    print(apply_crt_cycle_instructions(cycle_instructions))
